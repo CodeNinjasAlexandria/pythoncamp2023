eef8edab239c113a51a919b7a855f6f215b62cc1
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:50:29 -0400

    Delete madlibs.py

--- madlibs.py
+++
@@ -1,8 +0,0 @@
-store1 = input("give me a store")
-food1 = input("give me a food item")
-place1 = input("give me place")
-place2 = input("give me place")
-
-
-print ( " i went to the " + store1 + " to get a " + food1 + ". it tasted like a foot. ")
-print ( "then i went to my" + place1 + " to take a shower then i went to the " + place2 + "to have some fun. ") 
