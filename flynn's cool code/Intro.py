b7989ec074796fb5796669851e3b6dbe155b0d69
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:54:11 -0400

    Add files via upload

---
+++ flynn's cool code/Intro.py
@@ -0,0 +1,6 @@
+print ("Flynn brophy")
+# this is comment
+strechyPotato = ("thisPotatoIsYummy")
+stretchInt = (9002)
+
+print ("Look at this boring potato. " + strechyPotato + "BORINGWORDDS" + str(stretchInt) + "MORE BORING WORDS")
