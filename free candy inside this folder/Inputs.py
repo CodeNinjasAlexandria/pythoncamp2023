c1139064619584cbb17b7714f43bf8d609c052a7
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:44:21 -0400

    init commit

---
+++ free candy inside this folder/Inputs.py
@@ -0,0 +1,5 @@
+name = input("What is your social security number?")
+age = int(input("what are the funny little numbers on the back of your credit card?"))
+rating = int(input("what is your exact ip adress?"))
+
+print ("Your IP adress is " + name + " You are " + str(age) + " I now know where you live and your exact adress, and your credit card nubers :D" )
---
+++ free candy inside this folder/inro-benji.py
@@ -0,0 +1,5 @@
+print("There is free candy inside a van behind this building")
+#free money
+free_money = ("free money")
+stretchInt = (1974891249)
+print ("look at the van ouside the building near the trash can thats on fire. " + free_money +" GET TO THE FREE MONEY: " + str(stretchInt) + "plz get the free money") \
