f4f25c5ceca2ae4671d21497111b2643b034dd09
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:45:49 -0400

    Mateo

---
+++ camp/intro-Mateo.py
@@ -0,0 +1,6 @@
+print ( "I am good playing video games")
+#this is a comment
+stretchyPotatoYummy = "this potato is stechy"
+stretchInt = (0000)
+
+print ( "Look this potato it is cool. " + stretchyPotatoYummy + "Its is currently and it have a T-Shirt: more words " + str(stretchInt) + "even more words")
