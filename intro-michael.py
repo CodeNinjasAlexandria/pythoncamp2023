fc7b427a91bc8ee489d42ab5d0aa4b0d902bdfb8
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:50:21 -0400

    Delete intro-michael.py

--- intro-michael.py
+++
@@ -1,6 +0,0 @@
-print ("I like roblox, because super golf is cool!")
-# so golf is can be litte struggling
-stretchyPotatoYummy = ("this potato is stretchy")
-stretchInt = (9001)
-
-print ("Our potato. " + stretchyPotatoYummy + " How Long Is The Sting: " + str(stretchInt) + " Big stretchy potato")
