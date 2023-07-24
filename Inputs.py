bbae3f577b9aac62d81c805277b63879c11902d2
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:48:10 -0400

    Delete Inputs.py

--- Inputs.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your social security number?")
-age = int(input("what are the funny little numbers on the back of your credit card?"))
-rating = int(input("what is your exact ip adress?"))
-
-print ("Your IP adress is " + name + " You are " + str(age) + " I now know where you live and your exact adress, and your credit card nubers :D" )
