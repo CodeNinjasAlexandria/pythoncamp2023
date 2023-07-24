bfdda1ea0513ff2defda21c979c6728ee71e3815
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:42:49 -0400

    init commit

---
+++ Python Camp-Owen H/Mad libs.py
@@ -0,0 +1,9 @@
+Country1 = input("give me a country ")
+noun1 = input("give me a noun ")
+appliance1 = input("give me a appliance ")
+bodyPart1 = input("give me a body part ")
+noun2 = input("give me a noun ")
+
+print (" I went to " + Country1 + " to get a " + noun1 )
+print (" with the " + noun1 + " I also bought a " + appliance1 + " It made my " + bodyPart1 + " hurt " )
+print (" I went home with a full " + noun2 )
---
+++ Python Camp-Owen H/intro-Owen.py
@@ -0,0 +1,6 @@
+print("I'm 12")
+#this is a comment
+Small_Dog = ("the dog is big small")
+smallInt = (0.25)
+
+print(" look at this dog. " + Small_Dog +" It's smallness is currently: " + str(smallInt) + " that's very small")
