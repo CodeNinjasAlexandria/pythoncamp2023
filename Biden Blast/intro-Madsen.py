9ba97b7634210dde04d59b428e9a86f36cf8e1af
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:42:46 -0400

    init commit

---
+++ Biden Blast/intro-Madsen.py
@@ -0,0 +1,6 @@
+print ("Bernard is in my attic")
+#Save Bernart 
+JimmyJohns = ("jimmy is jonhs")
+YouMustSaveBernart = ("he's in my attic")
+
+print ("Bernard is your best friends" + JimmyJohns + "Bernard" + str(YouMustSaveBernart))
