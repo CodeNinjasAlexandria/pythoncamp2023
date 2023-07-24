4ac99a6998544e93f095135e8243ac7123159e21
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:42:56 -0400

    Init commit

---
+++ Python Camp Matthew/Intro - Matthew.py
@@ -0,0 +1,5 @@
+print ("I like to play videogames")
+#this is a comment
+stretchyPotatoYummy = ("this potato is stretchy")
+stretchInt = (2795)
+print ("Look at this potato. " + stretchyPotatoYummy +  " more words " + str(stretchInt) + "even more words")
---
+++ Python Camp Matthew/Mad lib.py
@@ -0,0 +1,9 @@
+country1 = input("Give me a country.")
+noun1 = input("Give me a noun.") 
+appliance1 = input("Give me a appliance")
+bodypart1 = input("Give me a bodypart")
+noun2 = input("Give me a noun")
+
+print("I went to " + country1 + " to get a noun1")
+print(" with the " + noun1 + " I also bought a "+ appliance1 + " It made my bodypart1 + " shake ")
+print(" I went home with a full " + noun2 ))
