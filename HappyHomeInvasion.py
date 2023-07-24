6d5169dbdbed2b4eecdb2b820e6a5efe68713d37
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:47:49 -0400

    Delete HappyHomeInvasion.py

--- HappyHomeInvasion.py
+++
@@ -1,8 +0,0 @@
-name = input("what security systems do you have?")n 
-name4 = (input("do you leave windows unlocked when you sleep?"))
-name5 = (input("where do you live?"))
-name1 = (input("What vaulables do you have near windows/exits in your home?"))
-name2 = (input("Do you have any weapons that could put off a home invader?"))
-name3 = (input("does home invasion sound fun?"))
-
-print ("Very good to know " + name + " You think  " + str(name3) + " This was 100 percent for educational reasons " )
