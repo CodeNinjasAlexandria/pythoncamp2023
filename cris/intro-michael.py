00c7dc172318dbce35774f4ac6020b0293385050
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:42:50 -0400

    init commit

---
+++ cris/intro-michael.py
@@ -0,0 +1,6 @@
+print("I am cool")
+#this is a comment
+stretchyPiazz = ("this piazz is stretchy")
+stretchInt = (9001)
+
+print ("look at this piazz. " + stretchyPiazz + " stretchIness " + str(stretchInt) + " even more stretchIness")
