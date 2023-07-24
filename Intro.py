98d572bab447a6a61e65c5ff084868a154b8eba4
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:49:12 -0400

    Delete Intro.py

--- Intro.py
+++
@@ -1,6 +0,0 @@
-print ("Flynn brophy")
-# this is comment
-strechyPotato = ("thisPotatoIsYummy")
-stretchInt = (9002)
-
-print ("Look at this boring potato. " + strechyPotato + "BORINGWORDDS" + str(stretchInt) + "MORE BORING WORDS")
