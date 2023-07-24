f88768d7df3c76f5d60f07787d5f27187b6fc9a8
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:42:26 -0400

    Add files via upload

---
+++ python stuff/intro-eamonn-1.py
@@ -0,0 +1,6 @@
+print ("I'm about to level up and you look like just enough xp")
+#save I'm bout to level up
+memesDnaOfSoul = ("Magnetic force Jack")
+stretchInt = 9001
+
+print ("Sir this is a wendy's" +memesDnaOfSoul + "what the heck is a sword")
---
+++ python stuff/le madlibs-1.py
@@ -0,0 +1,3 @@
+noun1 = input("give me a noun")
+place1 = input("give me a place")
+person1 = input("give me a person")
