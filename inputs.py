969c136cd2c1237428cf0908a4b7874330389677
CodeNinjasAlexandria <140417961+CodeNinjasAlexandria@users.noreply.github.com>
2023-07-24 15:49:42 -0400

    Delete inputs.py

--- inputs.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your name?") #string
-age = int(input("How old are you?"))
-rating = int(input("how cracked are you at fortnite from 1-10?"))
-
-print("Flynn, " + "you are " + str(age) + " years old! It looks like your gaming skills are a " + str(rating) + " out of 10" )
